d3d3c46ba7ebbd8319bcfd2276af6ddcff0c020b
Jose <rojasjose8888@gmail.com>
2015-05-14 19:36:22 -0430

    Commit inicial para el proyecto

---
+++ Biopsias/src/menu.java
@@ -0,0 +1,66 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
+
+
+public class menu {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					menu window = new menu();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public menu() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 449, 331);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnBasicos = new JMenu("Basicos");
+		menuBar.add(mnBasicos);
+		
+		JMenu mnMovimientos = new JMenu("Movimientos");
+		menuBar.add(mnMovimientos);
+		
+		JMenu mnReportes = new JMenu("Reportes");
+		menuBar.add(mnReportes);
+		
+		JMenu mnAyuda = new JMenu("Ayuda");
+		menuBar.add(mnAyuda);
+		
+		JMenuItem mntmAyuda = new JMenuItem("Ayuda");
+		mnAyuda.add(mntmAyuda);
+		
+		JMenuItem mntmSalir = new JMenuItem("Salir");
+		mnAyuda.add(mntmSalir);
+	}
+
+}
